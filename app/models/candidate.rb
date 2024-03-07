a778ebf423538f905a802625a2eca97fe44b997c
Beatriz Aranha <be.aranha@gmail.com>
2024-03-07 19:14:14 -0300

    Candidates Controllers started - terms & conditions corrected with new migration

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,9 +1,9 @@
 class Candidate < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   has_many :matches
   has_one_attached :photo
   has_one_attached :cv_upload
 
-  validates :first_name, :last_name, :birth_date, :address, :nationality, :gender, :tech_interest, :tech_languages, :cv_upload, presence: true
-  validates :terms_condition, acceptance: true
+  validates :first_name, :last_name, :birth_date, :address, :nationality, :gender, :tech_interest, :tech_languages, presence: true
+  # validates :terms_condition, acceptance: true
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 class Job < ApplicationRecord
-  belongs_to :company
+  belongs_to :company, dependent: :destroy
   has_many :matches
 
   validates :title, :job_description, :soft_skills, :programming_languages, :work_visa, :salary, :application_deadline, presence: true
