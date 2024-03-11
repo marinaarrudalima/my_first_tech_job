d2d8b2e4e9d130fd9cdf6b0168b5831cd6575288
Beatriz Aranha <be.aranha@gmail.com>
2024-03-11 16:05:55 -0300

    Cloudinary implemented in Candidate FORM

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -2,7 +2,7 @@ class Candidate < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_many :matches
   has_one_attached :photo
-  has_one_attached :resume
+  has_one_attached :CV_upload
 
   validates :first_name, :last_name, :birth_date, :address, :nationality, :gender, :tech_interest, :tech_languages, presence: true
   validates :terms_condition, acceptance: true
