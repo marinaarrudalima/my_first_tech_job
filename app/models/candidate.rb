7c45bbe717b1b5b423142b44a9777d6cd9bbc38e
Carolina Leone <carol.leone@hotmail.com>
2024-03-14 08:48:57 -0300

    mudancinhas

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -7,7 +7,7 @@ class Candidate < ApplicationRecord
   validates :first_name, :last_name, :birth_date, :address,
             :nationality, :gender, :tech_interest, :tech_languages, :cv_upload, presence: true
   validates :terms_condition, acceptance: true
-
+  validates :summary, length: { maximum: 100 }
   # Custom validation method, if you want to ensure that tech_interest and tech_languages only contain specific values
 
   INTEREST_OPTIONS = ['Web Development', 'Data Science', 'Cyber Security', 'UI/UX', 'Mobile Development', 'Product Management'].freeze
@@ -15,5 +15,4 @@ class Candidate < ApplicationRecord
 
   validates :tech_interest, inclusion: { in: INTEREST_OPTIONS }
   validates :tech_languages, inclusion: { in: LANGUAGES_OPTIONS }
-
 end
